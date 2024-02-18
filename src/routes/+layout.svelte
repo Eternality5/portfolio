2df4e6c847384fb59b3eb1a420fa134a1a976428
Eternality5 <83692139+Eternality5@users.noreply.github.com>
2024-02-18 16:14:28 +1030

    Created heading section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,13 @@
 <script>
+	import { AppShell } from '@skeletonlabs/skeleton';
+import '../app.pcss';
 	import Header from './Header.svelte';
-	import './styles.css';
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
+<AppShell slotSidebarLeft>
+	<Header slot="header" />
+	<div class="">
 		<slot />
-	</main>
-</div>
+	</div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,59 @@
 <script>
+	import {Avatar} from "@skeletonlabs/skeleton"
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="Home Page" content="Portfolio website for Maximilien Schuller" />
 </svelte:head>
 
-<section>
-	<h1>
-		Maximilien Schuller
-	</h1>
+<section class="text-middle mx-10 text-center">
+	<div class="flex justify-center py-2">
+		<Avatar src="avatar.jpg" width="w-64" rounded="rounded-full" initials="MS" />
+	</div>
+	<div class="py-2">
+		<h1 class="h1">MAXIMILIEN SCHULLER</h1>
+	</div>
+	<div class="py-2">
+		<p>
+			Hey there! I'm Max, a full-stack developer based in Adelaide with experience working with Java Spring Boot and
+			React with TypeScript.
+		</p>
+	</div>
+	<div class="flex justify-center">
+	<div class="fit-content logo-cloud grid-cols-1 gap-1 lg:!grid-cols-2 max-w-full justify-center">
+		<a class="logo-item p-4" href="https://github.com/Eternality5">
+			<img src="github.svg" alt="Github Logo" />
+			<span>Github</span>
+		</a>
+		<a class="logo-item p-4" href="https://www.linkedin.com/in/maximilien-schuller-477454174/">
+			<img src="linkedin.svg" alt="Linkedin Logo" />
+			<span>Linkedin</span>
+		</a>
+	</div>
+</div>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+<!-- <section>
+	<h2 class="h2">My Projects</h2>
 
-	h1 {
-		width: 100%;
-	}
+	<div class="grid grid-cols-1 lg:!grid-cols-3 content-start gap-4">
+		<a href="https://github.com/Eternality5/g1-serp2023" class="card p-4">
+			<header class="card-header">Deidentification Framework</header>
+			<section class="p-4">content</section>
+			<footer class="card-footer">By Max Schuller & Vinay Kumar</footer>
+		</a>
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+		<a href="https://github.com/Eternality5/g1-serp2023" class="card p-4">
+			<header class="card-header">header</header>
+			<section class="p-4">content</section>
+			<footer class="card-footer">footer</footer>
+		</a>
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+		<a href="https://github.com/Eternality5/g1-serp2023" class="card p-4">
+			<header class="card-header">header</header>
+			<section class="p-4">content</section>
+			<footer class="card-footer">footer</footer>
+		</a>
+	</div>
+</section> -->
